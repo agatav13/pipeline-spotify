bef7296d95bd6f8b4bc272fcbbfdfbfa655c96ac
agatav13 <agataomasta05@gmail.com>
2025-08-23 15:14:17 +0200

    test get_token() method

--- tests/test_spotify_api.py
+++ tests/test_spotify_api.py
@@ -1,5 +1,6 @@
 import base64
 import re
+
 import pytest
 
 from api.spotify_api import SpotifyAPI
@@ -23,8 +24,42 @@ def test_init_builds_base64_correctly(monkeypatch):
 
     client = SpotifyAPI()
 
-    expected = base64.b64encode("dummy_id:dummy_secret".encode()).decode()
+    expected = base64.b64encode(b"dummy_id:dummy_secret").decode()
     assert client.client_creds_b64 == expected
+
+
+def test_get_token_success(mocker):
+    client = SpotifyAPI()
+
+    fake_response = mocker.Mock()
+    fake_response.status_code = 200
+    fake_response.json.return_value = {
+        "access_token": "dummy_token",
+        "expires_in": 3600,
+    }
+
+    mocker.patch("requests.post", return_value=fake_response)
+
+    token = client.get_token()
+
+    assert token == "dummy_token"
+    assert client.access_token == "dummy_token"
+    assert client.expires_in == 3600
+
+
+def test_get_token_failure(mocker):
+    client = SpotifyAPI()
+
+    fake_response = mocker.Mock()
+    fake_response.status_code = 400
+    fake_response.text = "dummy_text"
+
+    mocker.patch("requests.post", return_value=fake_response)
+
+    with pytest.raises(
+        RuntimeError, match=re.escape("Failed to get token: 400 dummy_text")
+    ):
+        client.get_token()
 
 
 def test_get_headers_without_token():
